be4b6b42a7556c7b8ed6d80b554e4644ba5d4299
V. Hanuman Kumar <50360331+vhanumankumar@users.noreply.github.com>
2021-03-01 19:19:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
-app = Flask(__name__)
 model = pickle.load(open('USA_house.pkl', 'rb'))
-
+app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
